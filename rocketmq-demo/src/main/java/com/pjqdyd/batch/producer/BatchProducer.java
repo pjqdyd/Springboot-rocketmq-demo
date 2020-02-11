f33d795ee283de9e996c52b9460cc92dd1377005
pjqdyd <1909025079@qq.com>
2020-02-11 16:17:02 +0800

    批量消息

--- rocketmq-demo/src/main/java/com/pjqdyd/batch/producer/BatchProducer.java
+++ rocketmq-demo/src/main/java/com/pjqdyd/batch/producer/BatchProducer.java
@@ -3,7 +3,6 @@ package com.pjqdyd.batch.producer;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
