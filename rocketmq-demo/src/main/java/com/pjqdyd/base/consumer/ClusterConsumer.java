b936074883afa796da00ef9206020174e0a5ac3e
pjqdyd <1909025079@qq.com>
2020-02-11 15:04:20 +0800

    消息消费者

--- rocketmq-demo/src/main/java/com/pjqdyd/base/consumer/ClusterConsumer.java
+++ rocketmq-demo/src/main/java/com/pjqdyd/base/consumer/ClusterConsumer.java
@@ -1,20 +1,17 @@
 package com.pjqdyd.base.consumer;
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
-import org.apache.rocketmq.common.message.MessageExt;
-
-import java.util.List;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
 
 /**   
- * @Description:  [消息消费者]
+ * @Description:  [消息消费者, 负载均衡模式, 每个消费者消费部分消息]
  * @Author:       pjqdyd
  * @Version:      [v1.0.0]
  */
 
-public class Consumer {
+public class ClusterConsumer {
 
     public static void main(String[] args) throws Exception{
         //1. 创建消息消费者, 制定消费者组名
@@ -23,15 +20,14 @@ public class Consumer {
         consumer.setNamesrvAddr("192.168.141.129:9876");//2.指定NameServer地址
 
         //3.订阅主题Topic和Tag, 多个tag可以写成"tag1 || tag2", 或者 "*"
-        consumer.subscribe("base", "tag2");
+        consumer.subscribe("base", "tag1");
+
+        consumer.setMessageModel(MessageModel.CLUSTERING);//设置负载均衡消费模式(默认)
 
         //4.设置回调函数处理消息
-        consumer.registerMessageListener(new MessageListenerConcurrently() {
-            @Override
-            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
-                list.stream().forEach(msg -> System.out.println(new String(msg.getBody())));
-                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS; //消费完后,返回成功
-            }
+        consumer.registerMessageListener((MessageListenerConcurrently) (list, consumeConcurrentlyContext) -> {
+            list.stream().forEach(msg -> System.out.println(new String(msg.getBody())));
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS; //消费完后,返回成功
         });
 
         //5.启动消费者Consumer
